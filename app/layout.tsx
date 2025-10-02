60a3e6f0952d45f51816514516002c3c9c148f90
EgzonBllacaa <egzonbllaca9@gmail.com>
2025-10-02 14:19:28 +0200

    bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
-// import Navbar from "./Components/Navbar";
+// import Navbar from "./Components/";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
